de9bb8d2ede17f192a7cf2bb34fcbf864ebf04b0
ashallowperson <245951141+ashallowperson@users.noreply.github.com>
2026-07-11 06:21:56 +0500

    fix memory full crash

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 echo "- Skipping fenrir patches (already applied)"
+
+# Limit parallel build jobs to avoid OOM on 16c/30GB system
+export NINJA_NUM_JOBS=12
+export NINJA_HIGHMEM_NUM_JOBS=4
