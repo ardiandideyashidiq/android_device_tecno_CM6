9d334fd8afb5c07269948fd032e7bca68c4a3f1d
ashallowperson <245951141+ashallowperson@users.noreply.github.com>
2026-07-22 14:11:37 +0500

    Initialize the AOSP fingerprint service template
    Adds the fingerprint 2.x HAL skeleton (BiometricsFingerprint, Android.bp,
    init rc/xml, manifest) plus the props/vintf wiring for the UDFPS vendor
    HAL.

---
+++ fingerprint/2.3/service.cpp
@@ -0,0 +1,31 @@
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-jiiov-service"
+
+#include <android/log.h>
+#include <hidl/HidlSupport.h>
+#include <hidl/HidlTransportSupport.h>
+#include <android/hardware/biometrics/fingerprint/2.3/IBiometricsFingerprint.h>
+#include "BiometricsFingerprint.h"
+
+using android::hardware::biometrics::fingerprint::V2_3::IBiometricsFingerprint;
+using android::hardware::biometrics::fingerprint::V2_3::implementation::BiometricsFingerprint;
+using android::hardware::configureRpcThreadpool;
+using android::hardware::joinRpcThreadpool;
+using android::sp;
+
+int main() {
+    android::sp<IBiometricsFingerprint> bio = BiometricsFingerprint::getInstance();
+
+    configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+    if (bio != nullptr) {
+        if (::android::OK != bio->registerAsService()) {
+            return 1;
+        }
+    } else {
+        ALOGE("Can't create instance of BiometricsFingerprint, nullptr");
+    }
+
+    joinRpcThreadpool();
+
+    return 0;
+}
